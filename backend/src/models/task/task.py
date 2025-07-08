1b8b0d3e0666671af97d7186427c9187795ad2a2
ahmat1 <rahmatmahamatdout@gmail.com>
2025-07-08 12:06:06 +0200

    fix

---
+++ backend/src/models/task/task.py
@@ -0,0 +1,13 @@
+from app.extensions import db
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
