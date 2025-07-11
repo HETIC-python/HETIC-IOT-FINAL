d251a1de3bb5c47714c939eb1654d73311a8992c
Faithgg <fglindayi@gmail.com>
2025-07-11 21:07:10 +0200

    v1

--- backend/app.py
+++ backend/app.py
@@ -6,11 +6,11 @@ app = create_app()
 
 @app.route("/")
 def home():
-    a=18
     print("Home route accessed")
     return jsonify({"message": "Welcome to the Flask app! uwu"}), 200
 
 app.register_blueprint(workspace_bp, url_prefix="/api")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
