06db2928c659bd69981ac241c05c098c2f763d9d
jiad-saegus <jiad.abdul@saegus.com>
2025-08-22 14:44:17 +0200

    User upadate

--- backend/src/models/user/user.py
+++ backend/src/models/user/user.py
@@ -5,7 +5,7 @@ from src.extensions import db
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)  # Added username
@@ -13,15 +13,19 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     is_validated = db.Column(db.Boolean, default=False)  # Added for account validation
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    
+
     # Address fields
     address_line = db.Column(db.String(255), nullable=True)
     city = db.Column(db.String(100), nullable=True)
     postal_code = db.Column(db.String(20), nullable=True)
     country = db.Column(db.String(100), nullable=True)
 
+    role = db.Column(
+        db.Enum("admin", "user", name="user_roles"), default="user", nullable=False
+    )
+
     # TODO: Relationship with Order
     # orders = relationship("Order", back_populates="user", lazy='dynamic')
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f"<User {self.email}>"
