3cd28009180768fbbefa529f2b5e7ad72f99a2af
ROUCHAD AHMAT <rahmatmahamatdout@gmail.com>
2025-08-23 09:49:59 +0200

    Meteo api fix

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -6,7 +6,7 @@ from .sensor.sensor_routes import sensor_bp
 from .task.task_routes import task_bp
 from .user.user_routes import user_bp
 from .workspace.workspace_routes import workspace_bp
-
+from .weather.weather_route import weather_bp
 main = Blueprint("main", __name__)
 
 
@@ -18,3 +18,4 @@ def register_blueprints(app):
     app.register_blueprint(user_bp, url_prefix="/api")
     app.register_blueprint(workspace_bp, url_prefix="/api")
     app.register_blueprint(sensor_bp, url_prefix="/api")
+    app.register_blueprint(weather_bp, url_prefix="/api")
