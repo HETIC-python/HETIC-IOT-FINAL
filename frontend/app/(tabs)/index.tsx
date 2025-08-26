9c0a2961f058e4b3dbab09a478abe4d076b57baa
jiad-saegus <jiad.abdul@saegus.com>
2025-08-26 11:02:33 +0200

    Workspace added again on the dashboard

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,9 +1,7 @@
+import { SERVER_API_URL } from "@/config/api";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import {
-  ScrollView,
-  Text,
-  View
-} from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 type UserProfile = {
   email: string;
@@ -13,6 +11,23 @@ type UserProfile = {
 };
 
 export default function Index() {
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const token = localStorage.getItem("token");
+      const response = await fetch(`${SERVER_API_URL}/api/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) throw new Error("Failed to fetch workspaces");
+
+      return response.json();
+    },
+  });
+
+  console.log(data, isError, isLoading);
   return (
     <View className="flex-1 bg-white">
       <ScrollView contentContainerStyle={{ padding: 16 }}>
