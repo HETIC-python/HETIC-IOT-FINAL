fe7bdd9267b976818fcf6747dd2253b1a0aa078d
Doudou210 <siakakoudou.doumbia@yahoo.com>
2025-08-30 01:03:19 +0200

    Add Settings model to __all__ exports in models init file

---
+++ backend/src/models/settings/settings.py
@@ -0,0 +1,21 @@
+from src.extensions import db
+
+class Settings(db.Model):
+    __tablename__= "settings"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="SET NULL"), nullable=True)
+    email_notif = db.Column(db.Boolean, nullable=True)
+    mobile_notif = db.Column(db.Boolean, nullable=True)
+    theme_mode = db.Column(db.String(10), nullable=False, default="light")
+
+    def __repr__(self):
+        return f"<Settings user_id={self.user_id}>"
+    
+    def to__dict(self):
+        return {
+            "userId": self.user_id,
+            "emailNotif": self.email_notif,
+            "mobile": self.mobile_notif,
+            "themeMod": self.theme_mode
+        }
